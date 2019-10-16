3e77c26d746cca0c1c6df355c0da9debd436ab1d
Ghabriel Villarreal <ghabrielv@gmail.com>
2019-10-16 16:25:14 -0400

    Version 0.0.4

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var newWindow;
-exports.payButton = function(btc_address, env = undefined, port = 3000) {
+exports.payButton = function(btc_address, amount = undefined, money = undefined, description = undefined, env = undefined, port = 3000) {
 	var url = '';
 	switch (env) {
 		case undefined:
@@ -16,7 +16,8 @@ exports.payButton = function(btc_address, env = undefined, port = 3000) {
 		default:
 			env = 'prod';
 	}
-	var url = (env == 'prod' || env == 'qa') ? 'https://' + (env == 'prod' ? 'app.vitawallet.io' : 'qa-ui-dot-vita-wallet-api-qa-2.appspot.com') + '/pay-button/' + btc_address : 'http://localhost:' + port + '/pay-button/' + btc_address;
+	var params = btc_address + (amount ? '?amount=' + amount : '') + (money ? '?money=' + money : '') + (description ? '?description=' + description : '');
+	var url = (env == 'prod' || env == 'qa') ? 'https://' + (env == 'prod' ? 'app.vitawallet.io' : 'qa-ui-dot-vita-wallet-api-qa-2.appspot.com') + '/pay-button/' + params : 'http://localhost:' + port + '/pay-button/' + params;
 	newWindow = window.open(url,'Botón de pago','width=auto,height=auto,toolbar=0,menubar=0');  
 	if (window.focus) {
 		newWindow.focus();
