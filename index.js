8459a72044662aa2d1fe779fab8920387a02dc26
Ghabriel Villarreal <ghabrielv@gmail.com>
2019-10-16 20:35:52 -0400

    Version 0.0.5

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ exports.payButton = function(btc_address, amount = undefined, money = undefined,
 		default:
 			env = 'prod';
 	}
-	var params = btc_address + (amount ? '?amount=' + amount : '') + (money ? '?money=' + money : '') + (description ? '?description=' + description : '');
+	var params = btc_address + (amount ? '?amount=' + amount : '') + (money ? '&money=' + money : '') + (description ? '&description=' + description : '');
 	var url = (env == 'prod' || env == 'qa') ? 'https://' + (env == 'prod' ? 'app.vitawallet.io' : 'qa-ui-dot-vita-wallet-api-qa-2.appspot.com') + '/pay-button/' + params : 'http://localhost:' + port + '/pay-button/' + params;
 	newWindow = window.open(url,'Botón de pago','width=auto,height=auto,toolbar=0,menubar=0');  
 	if (window.focus) {
